8064b0330ea0269580e5a9b1279fc911e8672454
Robin Soosalu <robin.soosalu@gmail.com>
2021-03-15 18:38:31 +0200

    Initial commit.

--- src/App.svelte
+++ src/App.svelte
@@ -1,62 +1,37 @@
 <script lang="typescript">
-	import { onMount } from "svelte";
+	// import {io} from 'socket.io-client'
+	import MessageDisplay from './MessageDisplay.svelte'
+	import type {Message} from 'types/interfaces'
 
-	let count: number = 0;
-	onMount(() => {
-		const interval = setInterval(() => count++, 1000);
-		return () => {
-			clearInterval(interval);
-		};
-	});
+	// const socket = io('http://localhost:3000', {withCredentials: true, })
+	const user: string = 'Robin'
+	let message: string = ''
+	function submit(e: EventTarget & HTMLFormElement) {
+		e.preventDefault()
+		arr = [...arr, {message: message , username: 'Karl'}]
+		message = ''
+	}
+	let arr: Message[] = [{message: 'Hello frg9dxuu6gtbwe47p96tg89eptn4p7th6n9earp8g6ny98dra6ntg89e7ygiend', username: 'Paul'}, {message: 'Wassupwtg89ds7f6j9wp87 9tw9e86hg9wep86gb9pwe46gbo7ew44tn7op8e90yh45', username: 'Robin'}, {message: 'Fornite we about to get down', username: 'Paul'}]
 </script>
 
 <style>
-	:global(body) {
-		margin: 0;
-		font-family: Arial, Helvetica, sans-serif;
-	}
-	div code {
-		background: #0002;
-		padding: 4px 8px;
-		border-radius: 4px;
-	}
-	div p {
-		margin: 0.4rem;
-	}
-
-	div header {
-		background-color: #f9f6f6;
-		color: #333;
-		min-height: 100vh;
-		font-size: calc(10px + 2vmin);
-	}
-	div a {
-		color: #ff3e00;
-	}
-	div img {
-		height: 36vmin;
-		margin-bottom: 3rem;
-		animation: App-logo-spin infinite 1.6s ease-in-out alternate;
-	}
-	@keyframes App-logo-spin {
-		from {
-			transform: scale(1);
-		}
-		to {
-			transform: scale(1.06);
-		}
-	}
+.__text-container {
+	height: 95%;
+	padding-bottom: 5rem;
+}
 </style>
 
-<div class="text-center">
-	<header class="flex flex-col items-center justify-center">
-		<img src="/logo.svg" alt="logo" class="pointer-events-none" />
-		<p>Edit <code>src/App.svelte</code> and save to reload.</p>
-		<p>Page has been open for <code>{count}</code> seconds.</p>
-		<p>
-			<a href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
-				Learn Svelte
-			</a>
-		</p>
-	</header>
+<div>
+	<main class=' sm:container mx-auto bg-blue-50 rounded-md p-4 h-screen flex-col justify-between pb-10'>
+		<section class='__text-container mx-auto inline-flex flex-col overflow-y-auto w-full'>
+			{#each arr as sonum}
+				<MessageDisplay message={sonum} fromSelf={sonum.username === user} />
+			{/each}
+		</section>
+
+		<form class='flex object-bottom min-w-0 w-5/6 mx-auto mt-4' on:submit={e => submit(e)}>
+			<input bind:value={message} type="text" name="" id="message" class='bg-gray-100 rounded h-8 outline-none border-2 w-4/6 focus:border-blue-600 focus:bg-gray-50 px-2 py-3 mx-2'>
+			<button type='submit' class='bg-blue-500 text-white px-7 py-1 w-2/6 max-w-xs rounded hover:bg-blue-600'>Send</button>
+		</form>
+	</main>
 </div>
---
+++ src/MessageDisplay.svelte
@@ -0,0 +1,18 @@
+<script>
+    import type {Message} from '../types/interfaces'
+    export let fromSelf: boolean = true;
+    export let message: Message;
+</script>
+
+<!-- If sent message add 'self-end' to parent and add 'bg-blue' -->
+<article class={`__width rounded inline-block p-2 m-2 ${fromSelf ? 'self-end bg-blue-500' : 'bg-green-500'}`}>
+    <p class='text-white'>{message.message}</p>
+</article>
+
+<style>
+    .__width {
+        min-width: min-content;
+        word-wrap: normal;
+        max-width: 300px;
+    }
+</style>
